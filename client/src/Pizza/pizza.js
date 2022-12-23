a24099b8eca663607ae5d21f0bf9b0708b779805
dylangriess <dylangriess@gmail.com>
2022-12-22 17:08:32 -0700

    added toppings, sauces

--- client/src/Pizza/pizza.js
+++ client/src/Pizza/pizza.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+const sauces = ["marinara", "alfredo", "pesto", "bbq", "garlic oil"];
+const toppings = [
+  "pepperoni",
+  "sausage",
+  "mushrooms",
+  "onions",
+  "bell peppers",
+  "olives",
+  "pineapple",
+  "spinach",
+  "tomatoes",
+];
+
 function pizza() {
   return <div>pizza</div>;
 }
