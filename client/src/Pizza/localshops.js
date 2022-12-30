fe835f84a6cf7c9c97cb1a15730d4da9d5f177d8
dylangriess <dylangriess@gmail.com>
2022-12-29 17:46:28 -0700

    pizza shop names

--- client/src/Pizza/localshops.js
+++ client/src/Pizza/localshops.js
@@ -7,6 +7,14 @@ function localshops() {
   location = location_response.json();
   latitude = location["location"]["lat"];
   longitude = location["location"]["lng"];
+  maps_response = requests.get(
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=5000&type=pizza&key=YOUR_API_KEY"
+  );
+  pizza_shops = maps_response.json()["results"];
+
+  print(shop["name"]);
+  print(shop["vicinity"]);
+  print();
   return <div>localshops</div>;
 }
 
